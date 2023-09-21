883a73c14c641fd08026d841e761e898784a0626
Mariya Podosinova <mariya.podosinova@gmail.com>
2023-09-21 11:31:40 +0000

    xonstruct account form data source

--- src/main/java/smartdrive/TelemetrySource.java
+++ src/main/java/smartdrive/TelemetrySource.java
@@ -2,25 +2,48 @@ package smartdrive;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class TelemetrySource {
-  
 
-  public static Connection connect(){
+  public static Connection connect() {
     String db_file = "jdbc:sqlite:resources/customers.db";
     Connection connection = null;
-    
-    try{
-      connection = DriverManager.getConnection(db_file); 
+
+    try {
+      connection = DriverManager.getConnection(db_file);
       System.out.println("connection successful");
-    } catch(SQLException e){
+    } catch (SQLException e) {
       e.printStackTrace();
     }
     return connection;
-    
   }
-  public static void main(String[] args){
-    connect();
+
+  public static VehicleOwner getVehicleOwner(String username) {
+    String sql = "select * from customers where username = ?";
+    VehicleOwner vehicleOwner = null;
+    try (Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setString(1, username);
+      try (ResultSet resultSet = statement.executeQuery()) {
+        vehicleOwner = new VehicleOwner(
+            resultSet.getInt("id"),
+            resultSet.getString("name"),
+            resultSet.getString("username"),
+            resultSet.getString("password"),
+            resultSet.getInt("account_id"));
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return vehicleOwner;
+  }
+
+  public static void main(String[] args) {
+    VehicleOwner vehicleOwner1 = getVehicleOwner("twest8o@friendfeed.com");
+    System.out.println(vehicleOwner1.getName());
   }
 }
